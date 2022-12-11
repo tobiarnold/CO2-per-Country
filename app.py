ccf51df2d1a43fd8a439cad5d7a2f3377fa25a6e
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-12-11 10:25:56 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ def main():
                 height: 0%;
                 position: fixed;
                 }
-             div.block-container{padding-bottom:3rem;}
+             div.block-container{padding-bottom:1rem;}
            </style>
            """
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
